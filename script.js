edeb67acb326ac4d1a7dc6e104099f64fece0b9c
Gavin Meregillano <gavinmeregillano@gmail.com>
2025-03-13 15:58:35 -0700

    removed popularity filter

--- script.js
+++ script.js
@@ -212,9 +212,6 @@ fetch(url)
         Papa.parse(csvText, {
             header: true,
             complete: function (results) {
-                trackData = results.data.filter(track =>
-                    track.popularity && parseFloat(track.popularity) > 50
-                );
 
                 if (trackData && trackData.length > 0) {
                     displayTrackInfo(trackData[0]);
